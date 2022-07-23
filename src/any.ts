108b1ea17a80e9ab2ba1af0579b8151679243301
github user tmcoco0228si <proguteinitar3150@gmail.com>
2022-07-23 17:31:53 +0900

    add

--- src/any.ts
+++ src/any.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 export {};
+//型不定の場合に使用する
 
 let url: string =
   "https://udemy-utils.herokuapp.com/api/v1/articles?token=token123";
 
 //httpのGETメソッドを使えるようにしている。
+/**json形式のデータを取得する */
 axios.get(url).then(function (response) {
   //具体的なオブジェクトに対して型指定できる。
   interface Article {
--- src/tuple.ts
+++ src/tuple.ts
@@ -1,5 +1,6 @@
 export {};
 
+//要素の順番がわかっている場合の配列型指定
 //tuple型指定で型の順序を指定
 let profile: [string, number] = ["ham", 22];
 
