0a9df37b935bbc765a502251931986978086b037
github user tmcoco0228si <proguteinitar3150@gmail.com>
2022-07-26 19:27:33 +0900

    nullabuleのレクチャーをコミット

--- src/advanced-type/exclude-extract-nonnullable.ts
+++ src/advanced-type/exclude-extract-nonnullable.ts
@@ -3,16 +3,22 @@ export {};
 //関数の型指定
 type DebugType = () => void;
 
+//
 type SomeTypes = string | number | DebugType;
 
 //第一引数に削除したい型 実際に引数に消したい型を記載
 type FunctionType = Exclude<SomeTypes, string | number>;
 type NunFunctionType = Exclude<SomeTypes, DebugType>;
-type TypeExfludingFunction = Exclude<SomeTypes, Function>;
 
-//抽出する
+//大文字Functionで関数の型の総称、様々な関数の型を制限する。
+type TypeExcludingFunction = Exclude<SomeTypes, Function>;
+
+//抽出する 第二引数に指定した値だけ、抽出することができる。
 type FunctionTypeByExtract = Extract<SomeTypes, DebugType>;
-
 type NunFunctionTypeByExtract = Extract<SomeTypes, string | number>;
-
 type FunctionTypeExtractingFunction = Extract<SomeTypes, Function>;
+
+//nonnullable、ユニオン型からnull,undifinedを削除してくれる。
+
+type NullableTypes = string | number | null | undefined;
+type NonNullableTypes = NonNullable<NullableTypes>;
